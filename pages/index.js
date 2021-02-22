38aa8ac745a476a748ea9ddcd843902c6f362138
Ben Henning <19405113+Arcktosh@users.noreply.github.com>
2021-02-22 17:02:05 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,16 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
         </div>
+        <footer>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo"/>
+        </a>
+        </footer>
       </main>
     </>
   )
